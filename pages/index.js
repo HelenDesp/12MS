ec9f015187644cddfdfd3dc29ab0d6b0b78d56d7
HelenDesp <helenadespit@gmail.com>
2025-05-05 11:26:50 +0300

    Add files via upload

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,22 @@
 import React from "react";
 import Layout from "../components/Layout";
+import ConnectWallet from "../components/ConnectWallet";
+import WalletCard from "../components/WalletCard";
+import WalletInfoCard from "../components/WalletInfoCard";
+import TokenBalances from "../components/TokenBalances";
+import TokenTransfer from "../components/TokenTransfer";
+import Dashboard from "../components/Dashboard";
 
 const Home = () => {
   return (
     <Layout>
-      <div className="p-8">
-        <h1 className="text-4xl font-bold text-center text-blue-600">
-          ReVerse Genesis Wallet Starter is Working ✅
-        </h1>
-        <p className="mt-4 text-center text-gray-700">
-          Tailwind and Layout rendering confirmed.
-        </p>
+      <div className="p-4 space-y-6">
+        <ConnectWallet />
+        <WalletCard />
+        <WalletInfoCard />
+        <TokenBalances />
+        <TokenTransfer />
+        <Dashboard />
       </div>
     </Layout>
   );
