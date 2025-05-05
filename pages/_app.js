e02199b38aa0e7b9e541c5a425479db3a667e0ad
HelenDesp <helenadespit@gmail.com>
2025-05-05 12:00:39 +0300

    Add files via upload

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,40 @@
 import "../styles/globals.css";
-import { WagmiProvider, createConfig, http } from "wagmi";
-import { mainnet } from "wagmi/chains";
+import "@rainbow-me/rainbowkit/styles.css";
+import {
+  getDefaultWallets,
+  RainbowKitProvider,
+} from "@rainbow-me/rainbowkit";
+import { configureChains, createConfig, WagmiProvider } from "wagmi";
+import { mainnet, polygon, optimism, arbitrum, base } from "wagmi/chains";
+import { publicProvider } from "wagmi/providers/public";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-const config = createConfig({
-  chains: [mainnet],
-  transports: {
-    [mainnet.id]: http(),
-  },
+const { chains, publicClient } = configureChains(
+  [mainnet, polygon, optimism, arbitrum, base],
+  [publicProvider()]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: "12MS",
+  projectId: "8da6b8ad123727e26ef99477f407427a", // Replace with real ID from WalletConnect Cloud
+  chains,
+});
+
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors,
+  publicClient,
 });
 
 const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }) {
   return (
-    <WagmiProvider config={config}>
+    <WagmiProvider config={wagmiConfig}>
       <QueryClientProvider client={queryClient}>
-        <Component {...pageProps} />
+        <RainbowKitProvider chains={chains}>
+          <Component {...pageProps} />
+        </RainbowKitProvider>
       </QueryClientProvider>
     </WagmiProvider>
   );
