b75031e35a115108ca76e58666c816254c1ca6e3
HelenDesp <helenadespit@gmail.com>
2025-05-05 11:46:55 +0300

    Add files via upload

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,25 @@
+import React from "react";
 import Layout from "../components/Layout";
+import ConnectWallet from "../components/ConnectWallet";
+import WalletCard from "../components/WalletCard";
+import WalletInfoCard from "../components/WalletInfoCard";
+import TokenBalances from "../components/TokenBalances";
+import TokenTransfer from "../components/TokenTransfer";
+import Dashboard from "../components/Dashboard";
 
-export default function Home() {
+const Home = () => {
   return (
     <Layout>
-      <div className="p-8 text-center">
-        <h1 className="text-3xl font-bold text-green-500">Layout Loaded ?</h1>
-        <p className="text-gray-600 mt-2">If you see this, Layout is rendering children.</p>
+      <div className="p-4 space-y-6">
+        <ConnectWallet />
+        <WalletCard />
+        <WalletInfoCard />
+        <TokenBalances />
+        <TokenTransfer />
+        <Dashboard />
       </div>
     </Layout>
   );
-}
+};
+
+export default Home;
