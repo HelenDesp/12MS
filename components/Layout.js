d4e0fbaf2f5c2febc0cff2fe0fc2d2813031cb39
HelenDesp <helenadespit@gmail.com>
2025-05-05 11:45:58 +0300

    Add files via upload

--- components/Layout.js
+++ components/Layout.js
@@ -1,7 +1,9 @@
-import React from "react";
-
-const Layout = () => {
-  return <div>Layout</div>;
+const Layout = ({ children }) => {
+  return (
+    <div className="min-h-screen bg-gray-50 text-gray-900">
+      <main className="max-w-4xl mx-auto p-4">{children}</main>
+    </div>
+  );
 };
 
 export default Layout;
