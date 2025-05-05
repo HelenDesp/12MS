b07b7a307719bd38dabcf4e0ea724651a2bbe472
HelenDesp <helenadespit@gmail.com>
2025-05-05 11:51:07 +0300

    Add files via upload

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,25 @@
-import { useEffect, useState } from "react";
 import "../styles/globals.css";
+import { WagmiProvider, createConfig, http } from "wagmi";
+import { mainnet } from "wagmi/chains";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const config = createConfig({
+  chains: [mainnet],
+  transports: {
+    [mainnet.id]: http(),
+  },
+});
+
+const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
 }
 
 export default MyApp;
